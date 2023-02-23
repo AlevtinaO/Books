7b444dd3b73c4143d3d7d7ab67962e72b884ec98
olkhovskikh@bk.ru <olkhovskikh@bk.ru>
2023-02-23 22:25:20 +0300

    Начало

---
+++ script.js
@@ -0,0 +1,41 @@
+const books [
+    {
+        title: "Путешествие на Кон-Тики",
+        authors: "Тур Хейердал",
+        year: "2022",
+        publish: "'АСТ'",
+        image: "images/1.jpg"
+    },
+
+    {
+        title: "Ра",
+        authors: "Тур Хейердал",
+        year: "2020",
+        publish: "'АСТ'",
+        image: "images/2.jpg"  
+    },
+
+    {
+       title: "Аку Аку",
+        authors: "Тур Хейердал",
+        year: "2021",
+        publish: "'АСТ'",
+        image: "images/5.jpg"  
+    },
+
+    {
+        title: "Мальдивская загадка",
+        authors: "Тур Хейердал",
+        year: "2021",
+        publish: "'АСТ'",
+        image: "images/4.jpg"
+    },
+
+    {
+        title: "Экспедиция 'Тигрис'",
+        authors: "Тур Хейердал",
+        year: "2015",
+        publish: "Амфора",
+        image: "images/3.jpg"
+    }
+]
